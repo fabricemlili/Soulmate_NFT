73abdafd78df331f80a36a7ebdba5d1377953074
cfelgines <chloe.felgines@epfedu.fr>
2022-11-08 10:56:41 +0100

    final nft

--- nft-test/hardhat.config.js
+++ nft-test/hardhat.config.js
@@ -1,15 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-require('dotenv').config()
-
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  networks: { mumbai: {
-    url: process.env.MUMBAI_URL || "",
-    accounts:
-      process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-
+  networks: {
+    mumbai: {
+      url: "https://polygon-mumbai.g.alchemy.com/v2/xPACxJYzW3ovyVZS_NMuOHO-BZaKcF2d",
+      accounts:["3793d6cfddc42525668088ed09bc23b119da00cecf2567e82572a05e3a1774a7"
+      ],
+    },
   },
-}
 };
---
+++ nft-test/scripts/mint.js
@@ -0,0 +1,26 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+
+  const NFTEvent = await hre.ethers.getContractFactory("MyNFT");
+  const nFTevent = await NFTEvent.attach("0xf3f74442fde851e6849f8fe9a98649d7e60c3922");
+
+  await nFTevent.mintNFT("0x44Dc68892A17ea60E98dB3194AE8667DDDeC6900", "https://gateway.pinata.cloud/ipfs/QmXLeDjwiFc2fmR3SRTsgX4j3YQ1i9feeETmbtSWdZPMHT");
+
+  console.log(
+    ` deployed to ${nFTevent.address}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
